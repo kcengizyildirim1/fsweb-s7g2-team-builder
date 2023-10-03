61b142e5217e5be0c585012343fe7c1d81dacc0f
Kursad Cengiz Yildirim <k.cengizyildirim@gmail.com>
2023-10-03 16:41:21 +0300

    project init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [teamMember, setTeamMember] = useState([]);
+  return (
+    <div className="App">
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
